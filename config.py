fa7928e824516cb8639e0956cb25c09c181f69be
Nickel18 <167849937+Nickel18@users.noreply.github.com>
2024-04-24 03:19:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = API_ID = 20963688
 API_HASH = os.environ.get("API_HASH", "964a26dc404aa5e12dfe82ea94ae7ae3
 ")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7064782752:AAGvY_v7N5StrToJc6l15bJPKAZNy0gIguo")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7189162443:AAGSv4KRyaUwTdX1RpbZRKJPMZLwvhh0xI0")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
