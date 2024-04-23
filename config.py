f78efabfd0c99dd433579296c02a641c206823ef
Nickel18 <167849937+Nickel18@users.noreply.github.com>
2024-04-24 03:33:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 API_ID = API_ID = 20963688
 
-API_HASH = os.environ.get("API_HASH", "964a26dc404aa5e12dfe82ea94ae7ae3
-")
+API_HASH = os.environ.get("API_HASH", "964a26dc404aa5e12dfe82ea94ae7ae3")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7189162443:AAGSv4KRyaUwTdX1RpbZRKJPMZLwvhh0xI0")
 
